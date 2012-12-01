b38fc74d1d13bcb085ff9254f2e0c4e4af5b7c15
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2012-12-01 10:36:46 -0500

    Guid function added.
    * GUID function added.
    * Minor renames

--- classes/Util.php
+++ classes/Util.php
@@ -24,5 +24,47 @@ class Util
     {
         // TODO: use ini_parse and allow params to be used as constants in ini
     }
+
+    public static function guid($namespace='')
+    {
+        // TODO: use uuid_create also if available
+        if(function_exists('com_create_guid'))
+        {
+            return trim(com_create_guid(), '{}');
+        }
+        else
+        {
+            return static::internal_guid($namespace);
+        }
+    }
+
+    public static function internal_guid($namespace='')
+    {
+        // This code comes from http://php.net/manual/en/function.uniqid.php
+        // Curly brackets are not included
+        static $guid = '';
+
+        $uid = uniqid("", true);
+        $data = $namespace;
+        $data .= $_SERVER['REQUEST_TIME'];
+        $data .= $_SERVER['HTTP_USER_AGENT'];
+        $data .= $_SERVER['LOCAL_ADDR'];
+        $data .= $_SERVER['LOCAL_PORT'];
+        $data .= $_SERVER['REMOTE_ADDR'];
+        $data .= $_SERVER['REMOTE_PORT'];
+
+        $hash = strtoupper(hash('ripemd128', $uid . $guid . md5($data)));
+        $guid = substr($hash,  0,  8) .
+            '-' .
+            substr($hash,  8,  4) .
+            '-' .
+            substr($hash, 12,  4) .
+            '-' .
+            substr($hash, 16,  4) .
+            '-' .
+            substr($hash, 20, 12);
+
+        return $guid;
+    }
 }
 
